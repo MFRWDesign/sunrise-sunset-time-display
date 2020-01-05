3e9e6951d098e50bdb3640c4e3eda848abd95cf9
Thomas Sample <thomas@mfrwdesign.com>
2020-01-05 15:51:46 -0500

    Add jQuery .ready() handler and init table with hard-coded data

---
+++ application.js
@@ -0,0 +1,7 @@
+// Handler for .ready() -- A shorthand here is usually used:
+// $(function() {       -- This line is exactly the same as the next one:
+$( document ).ready(function() {
+  $( "#date" ).text( "1-5-2020" );
+  $( "#sunrise" ).text( "5:00AM EST" );
+  $( "#sunset" ).text( "5:00AM EST" );
+});

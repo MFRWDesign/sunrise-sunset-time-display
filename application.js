3ca9ac1ecf33a3a782fb0a354c26e00066588aec
Thomas Sample <thomas@mfrwdesign.com>
2020-01-05 15:58:49 -0500

    Use a real date instead of a hard coded one

--- application.js
+++ application.js
@@ -1,7 +1,8 @@
 // Handler for .ready() -- A shorthand here is usually used:
 // $(function() {       -- This line is exactly the same as the next one:
 $( document ).ready(function() {
-  $( "#date" ).text( "1-5-2020" );
+  var date = new Date();
+  $( "#date" ).text( date );
   $( "#sunrise" ).text( "5:00AM EST" );
   $( "#sunset" ).text( "5:00AM EST" );
 });
